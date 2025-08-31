dd2b4fc3d03160df413b38fd0ad16387089283a4
Loganathan Ponnusamy <ponnlogan@gmail.com>
2025-08-31 10:20:40 -0700

    updating files

--- test3a.py
+++ test3a.py
@@ -1,2 +1,19 @@
 for x in range(10)
+
+
 	print(x)
+
+
+
+print(X)
+print("Hello")print("Hello")
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+print("Hello")
+print("Hello")
+
